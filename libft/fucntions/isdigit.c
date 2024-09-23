a781da31482cd9664ce38a06a89963705912f064
Antonibaldo <antonibaldomarcos22@gmail.com>
2024-09-23 19:41:21 +0200

    gg

--- libft/fucntions/isdigit.c
+++ libft/fucntions/isdigit.c
@@ -1,39 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalpha.c                                          :+:      :+:    :+:   */
+/*   isdigit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abaldo-m <abaldo-m@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/18 15:22:36 by abaldo-m          #+#    #+#             */
-/*   Updated: 2024/09/23 19:19:59 by abaldo-m         ###   ########.fr       */
+/*   Created: 2024/09/23 19:29:44 by abaldo-m          #+#    #+#             */
+/*   Updated: 2024/09/23 19:36:37 by abaldo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <unistd.h>
 
-int	is_alpha(int c)
+int	is_digit(int c)
 {
 	if (c == '\0')
 	{
 		return (0);
 	}
-	if (!((c >= 'A' && c <= 'Z')
-			|| (c >= 'a' && c <= 'z')))
-	{
+	if (!(c >= '0' && c <= '9'))
 		return (0);
-	}
 	return (1);
 }
 
 int main(void)
 {
-	char c = 'g';
-	if (is_alpha(c))
+	char c = '0';
+	if (is_digit(c))
 		printf("%s", "tu madre");
 	else
 		printf("%s", "tu padre");
 
 	return(0);
 }
-
