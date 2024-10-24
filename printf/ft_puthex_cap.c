10834357ddc74f04e1fd34fac38ffcedd1aef78c
Antonibaldo <antonibaldomarcos22@gmail.com>
2024-10-24 08:40:16 +0200

    all norm

--- printf/ft_puthex_cap.c
+++ printf/ft_puthex_cap.c
@@ -6,21 +6,21 @@
 /*   By: abaldo-m <abaldo-m@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:24:25 by abaldo-m          #+#    #+#             */
-/*   Updated: 2024/10/23 17:27:01 by abaldo-m         ###   ########.fr       */
+/*   Updated: 2024/10/24 08:35:51 by abaldo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int     ft_puthex_cap(unsigned long long num)
+int	ft_puthex_cap(unsigned long long num)
 {
-        int             len;
-        char    hex_digit;
+	int		len;
+	char	hex_digit;
 
-        len = 0;
-        hex_digit = "0123456789ABCDEF"[num % 16];
-        if (num >= 16)
-                len += ft_puthex_cap(num / 16);
-        len += ft_putchar(hex_digit);
-        return (len);
+	len = 0;
+	hex_digit = "0123456789ABCDEF"[num % 16];
+	if (num >= 16)
+		len += ft_puthex_cap(num / 16);
+	len += ft_putchar(hex_digit);
+	return (len);
 }
