79ea16c4e4fe66a716323e06f6e68a9ae94f86e0
Antonibaldo <antonibaldomarcos22@gmail.com>
2025-01-13 20:02:23 +0100

    arreglo1

--- fdf/fdf.h
+++ fdf/fdf.h
@@ -6,14 +6,14 @@
 /*   By: abaldo-m <abaldo-m@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 16:42:33 by abaldo-m          #+#    #+#             */
-/*   Updated: 2025/01/13 19:13:32 by abaldo-m         ###   ########.fr       */
+/*   Updated: 2025/01/13 20:01:55 by abaldo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include "minilibx-linux/mlx.h"
+# include "mlx.h"
 # include "libft.h"
 # include "get_next_line.h"
 # include "ft_printf.h"
