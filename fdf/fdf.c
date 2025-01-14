8857f693e039a0fa8f35a9da891aecf9aa578130
Antonibaldo <antonibaldomarcos22@gmail.com>
2025-01-14 21:02:34 +0100

    few things

--- fdf/fdf.c
+++ fdf/fdf.c
@@ -6,7 +6,7 @@
 /*   By: abaldo-m <abaldo-m@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 16:40:00 by abaldo-m          #+#    #+#             */
-/*   Updated: 2025/01/13 19:52:59 by abaldo-m         ###   ########.fr       */
+/*   Updated: 2025/01/14 21:02:13 by abaldo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ int	main(int argc, char **argv)
 	{
 		// 1. leer el archivo .fdf, donde esta el mapa de numeros.
 		handle_map(argv[1]);
+		// 1.1 Chequear el formato, que todas la lineas sean iguales y 
+		// que no haya cosas qeu no sean numeros, positivos o negatvios
+		check_format(argv[1]);
 		// 2. pasar de 3D a 2D las coordenadas guardadas.
 		// 3. poner los puntos en la pantalla.
 		// 4. programar el algoritmo DDA para conectar los puntos.
